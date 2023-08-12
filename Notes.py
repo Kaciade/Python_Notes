ebb56f686d04abcbe3709968692f3f37a459ccf5
Kaciade <valerypolyakov60@gmail.com>
2023-08-12 21:08:25 +0200

    Добавлены  возможности сохдания и удаления заметок

--- Notes.py
+++ Notes.py
@@ -7,3 +7,13 @@ class Note:
         self.title = title
         self.body = body
         self.timestamp = datetime.now()
+
+class NoteManager:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def delete_note(self, note_id):
+        self.notes = [note for note in self.notes if note.note_id != note_id]
