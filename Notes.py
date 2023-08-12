48214f14190538873063a48cac2a6c90e8bbe55d
Kaciade <valerypolyakov60@gmail.com>
2023-08-12 22:18:25 +0200

    Добавлена рабочая часть программы

--- Notes.py
+++ Notes.py
@@ -57,3 +57,47 @@ class NoteManager:
             )
             note.timestamp = datetime.strptime(note_data['timestamp'], '%Y-%m-%d %H:%M:%S')
             self.notes.append(note)
+
+def main():
+    note_manager = NoteManager()
+    note_manager.load_notes_from_file('notes.json')
+
+    while True:
+        print('1. Добавить заметку')
+        print('2. Удалить заметку')
+        print('3. Редактировать заметку')
+        print('4. Просмотреть заметки по дате')
+        print('5. Сохранить изменения')
+        print('6. Выйти из программы')
+        choice = input('Введите номер операции: ')
+
+        if choice == '1':
+            note_id = input('Введите идентификатор заметки: ')
+            title = input('Введите заголовок заметки: ')
+            body = input('Введите текст заметки: ')
+            note = Note(note_id, title, body)
+            note_manager.add_note(note)
+        elif choice == '2':
+            note_id = input('Введите идентификатор заметки для удаления: ')
+            note_manager.delete_note(note_id)
+        elif choice == '3':
+            note_id = input('Введите идентификатор заметки для редактирования: ')
+            new_title = input('Введите новый заголовок заметки: ')
+            new_body = input('Введите новый текст заметки: ')
+            note_manager.edit_note(note_id, new_title, new_body)
+        elif choice == '4':
+            date_str = input('Введите дату для фильтрации заметок (в формате ГГГГ-ММ-ДД): ')
+            date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            filtered_notes = note_manager.filter_notes_by_date(date)
+            for note in filtered_notes:
+                print(f'Заметка {note.note_id}: {note.title} - {note.body} ({note.timestamp})')
+        elif choice == '5':
+            note_manager.save_notes_to_file('notes.json')
+            print('Изменения сохранены.')
+        elif choice == '6':
+            break
+        else:
+            print('Некорректный выбор. Попробуйте еще раз.')
+
+if __name__ == '__main__':
+    main()
